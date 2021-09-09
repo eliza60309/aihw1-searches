59ec7c45b5b014404c2bd939c866a92234fb8d66
eliza60309 <eliza60309@gmail.com>
2021-09-09 18:32:58 +0800

    bfs done I think I guess I hope there is no bug

--- homework.cpp
+++ homework.cpp
@@ -1,44 +1,140 @@
 #include <string>
 #include <fstream>
 #include <map>
+#include <vector>
+#include <queue>
+#include <set>
+#include <iostream>
+
 using namespace std;
 
 class node
 {
 	public:
 	node go(int);
+	int go(node);
 	node set(int, int, int);
 	int x, y, z;
+	bool operator==(const node n)
+	{
+		if(x == n.x && y == n.y && z == n.z)
+			return true;
+		else 
+			return false;
+	}
+};
 
+bool operator<(node m, node n)
+{
+	if(m.x < n.x)
+		return true;
+	if(m.x > n.x)
+		return false;
+	if(m.y < n.y)
+		return true;
+	if(m.y > n.y)
+		return false;
+	if(m.z < n.z)
+		return true;
+	return false;
+	
+}
+
+class state
+{
+	public:
+	state(node n)
+	{
+		at = n;
+		sum = 0;
+	}
+	vector<int> list;
+	node at;
+	int sum;
 };
 
 int main()
 {
 	string s;
-
-	ifstream cin;
-	ofstream cout;
-	cin.open("input.txt", ifstream::in);
-	cout.open("output.txt", ofstream::out);
-	cin >> s;
+	ifstream in;
+	ofstream out;
+	in.open("input.txt", ifstream::in);
+	out.open("output.txt", ofstream::out);
+	in >> s;
 	int dim_x, dim_y, dim_z;
-	int begin_x, begin_y, begin_z;
-	int end_x, end_y, end_z;
-	cin >> dim_x >> dim_y >> dim_z;
-	cin >> begin_x >> begin_y >> begin_z;
-	cin >> end_x >> end_y >> end_z;
+	node begin, end;
+	in >> dim_x >> dim_y >> dim_z;
+	in >> begin.x >> begin.y >> begin.z;
+	in >> end.x >> end.y >> end.z;
 	int N;
-	cin >> N;
-	map <node, *node> adj;
+	in >> N;
+	map <node, vector<int> > adj;
+	int inv[19] = {0, 2, 1, 4, 3, 6, 5, 10, 9, 8, 7, 14, 13, 12, 11, 18, 17, 16, 15};
+	int c1[19] = {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+	int c2[19] = {0, 10, 10, 10, 10, 10, 10, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14};
+	int *cost;
+	if(s == "BFS")
+		cost = c1;
+	else
+		cost = c2;
 	while(N--)
 	{
 		node n;
-		cin >> n.x 
+		in >> n.x;
+		in >> n.y;
+		in >> n.z;
+		while(in.peek() != '\n' && in.peek() != EOF)
+		{
+			int i;
+			in >> i;
+			adj[n].push_back(i);
+		}
 	}
+	set<node> traveled;
+	queue<state> frontier;
+	frontier.push(state(begin));
+	while(!frontier.empty())
+	{
+		state now_state = frontier.front();
+		frontier.pop();
+		if(now_state.at == end)
+		{
+			if(s == "BFS")
+			{
+				out << now_state.sum << endl;
+				out << now_state.list.size() + 1 << endl;
+				out << begin.x << " " << begin.y << " " << begin.z << " " << 0;
+				node travel = begin;
+				for(int i = 0; i < now_state.list.size(); i++)
+				{
+					travel = travel.go(now_state.list[i]);
+					out << endl;
+					out << travel.x << " " << travel.y << " " << travel.z << " ";
+					out << cost[now_state.list[i]];
+				}
+			}
+			if(s == "UCS")
+			{
+				
+			}
+			return 0;
+		}
+		vector<int> *vect = &adj[now_state.at];
+		for(int i = 0; i < vect->size(); i++)
+		{
+			node target = now_state.at.go((*vect)[i]);
+			if(traveled.count(target))
+				continue;
+			state s = now_state;
+			s.sum += cost[(*vect)[i]];
+			s.list.push_back((*vect)[i]);
+			s.at = target;
+			traveled.insert(s.at);
+			frontier.push(s);
+		}
 
-	
-
-
+	}
+	out << "FAIL";
 	return 0;
 }
 
@@ -48,6 +144,17 @@ node node::set(int in_x, int in_y, int in_z)
 	this->y = in_y;
 	this->z = in_z;
 	return *this;
+}
+
+int node::go(node n)
+{
+	int dx = this->x - n.x;
+	int dy = this->y - n.y;
+	int dz = this->z - n.z;
+	int len = dx * dx + dy * dy + dz * dz;
+	if(len == 2)return 14;
+	if(len == 1)return 10;
+	else return -1;
 }
 
 node node::go(int step)
@@ -115,3 +222,4 @@ node node::go(int step)
 	n.z += this->z;
 	return n;
 }
+
